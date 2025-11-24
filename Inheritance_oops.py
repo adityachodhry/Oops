358bd4e9da394e707d3e95768f4c16d2093b44ed
adityachodhry <adityachy02131@gmail.com>
2025-11-24 16:17:52 +0530

    For Oops Practices

---
+++ Inheritance_oops.py
@@ -0,0 +1,24 @@
+# Inheritance
+
+class Student:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    
+    def __str__(self):
+        return f"Student Name: {self.first_name} {self.last_name}"
+
+class Teacher(Student):
+    def __init__(self, first_name, last_name, s_marks, s_section):
+        super().__init__(first_name, last_name)
+        self.s_marks = s_marks
+        self.s_section = s_section
+    
+    def __str__(self):
+        return f"Student Name: {self.first_name} {self.last_name}, marks: {self.s_marks}, section: {self.s_section}"
+    
+# s1 = Student("Aditya", "Choudhary")
+# print(s1)
+
+s2 = Teacher("Tushar", "Jha", 90, "A")
+print(s2)
---
+++ oops.py
@@ -0,0 +1,35 @@
+# Class in Python
+# Objects in Python
+# Polymorphism in Python
+# Encapsulation in Python
+# Inheritance in Python
+# Data Abstraction in Python
+
+class Student:
+    # __init__ is an constructor runs automatically when an object is created.
+
+    def __init__(self, name, email, username, password, number):
+        self.name = name
+        self.email = email
+        self.username = username
+        self.__password = password  # private attribute
+        self.number = number
+
+    def display_student_info(self):
+        print("Student Information:")
+        print("Name:", self.name)
+        print("Age:", self.email)
+        print("Username:", self.username)
+        print("Contact Number:", self.number)
+
+# Create a student object
+name = str(input("Enter your name: "))
+email = str(input("Enter your email ID: "))
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+number = int(input("Enter your contact number: "))
+
+student1 = Student(name, email, username, password, number)
+
+# Display student information
+student1.display_student_info()
